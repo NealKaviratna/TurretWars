eb55508656f28fd7cd23526f306172dfff1143ba
Neal Kaviratna <Nkaviratna@gatech.edu>
2016-05-17 14:00:02 -0400

    Started Turret + Projectiles

---
+++ Turret Wars/Assets/Scripts/Turret/IWeapon.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class IWeapon : MonoBehaviour {
+
+    private int level;
+    private float fireRate;
+
+    private BulletBehaviour bulletBehaviour;
+
+    public abstract void Fire();
+}
