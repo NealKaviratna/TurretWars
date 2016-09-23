fa7c4a6df909dc0714a7773fb1f1239e0db4464f
echen67 <chenemily3@yahoo.com>
2016-09-23 02:01:07 -0400

    menu

---
+++ Turret Wars/Assets/Scripts/Exit.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Exit : MonoBehaviour {
+
+    void OnMouseEnter()
+    {
+        transform.localScale += new Vector3(0.1f, 0, 0);
+    }
+
+    void OnMouseExit()
+    {
+        transform.localScale -= new Vector3(0.1f, 0, 0);
+    }
+
+    void OnMouseDown()
+    {
+        Application.Quit();
+    }
+}
---
+++ Turret Wars/Assets/Scripts/NewGame.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class NewGame : MonoBehaviour {
+
+	void OnMouseEnter()
+    {
+        transform.localScale += new Vector3(0.1f, 0, 0);
+    }
+
+    void OnMouseExit()
+    {
+        transform.localScale -= new Vector3(0.1f, 0, 0);
+    }
+
+    void OnMouseDown()
+    {
+        SceneManager.LoadScene("TurretTest");
+    }
+}
