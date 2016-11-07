88c1e05417400c8af9a2a5df0ba1daf8f567e0ee
Neal Kaviratna <Nkaviratna@gatech.edu>
2016-11-07 13:01:49 -0500

    Add auto bullets to menu

---
+++ Turret Wars/Assets/Scripts/Menu/MenuBulletBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Simple Bullet Behaviour for use with Machine Gun and potentially Super Shotgun
+/// </summary>
+public class MenuBulletBehaviour : BulletBehaviour
+{
+    public override Poolable Create(Player player, uint id, Vector3 pos)
+    {
+        base.Create(player, id, pos);
+        this.timer = 10.0f;
+        return this;
+    }
+
+    public override string ToString()
+    {
+        return "MenuBullet";
+    }
+
+    void OnCollisionEnter(Collision coll)
+    {
+        if (this.timer < 9.95f)
+            this.Die();
+    }
+}
