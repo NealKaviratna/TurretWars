68a0043bfb5dbe1fd3d059d4b0b9dd3cd4811072
Neal Kaviratna <Nkaviratna@gatech.edu>
2016-11-16 19:44:36 -0500

    Pulled back fixed UI from averys old commit

--- Turret Wars/Assets/Scripts/Turret/Weapons/HellfireMissileLauncher.cs
+++ Turret Wars/Assets/Scripts/Turret/Weapons/HellfireMissileLauncher.cs
@@ -15,6 +15,8 @@ public class HellfireMissileLauncher : Weapon
 
     private float timer;
 
+    private Image reticle;
+
     // Use this for initialization
     protected override void Awake()
     {
@@ -26,6 +28,7 @@ public class HellfireMissileLauncher : Weapon
         missilePool = new ObjectPool<HellfireMissileBehaviour>(DummyGameObject);
 
         this.upgradeUISetup();
+        this.reticle = GameObject.Find("UI:Crosshair").GetComponent<Image>();
 
         base.Awake();
     }
@@ -43,7 +46,7 @@ public class HellfireMissileLauncher : Weapon
 
     public override void Fire()
     {
-        RaycastHit hitInfo; ;
+        RaycastHit hitInfo;
 
         List<HellfireMissileBehaviour> missiles = new List<HellfireMissileBehaviour>();
 
@@ -51,7 +54,8 @@ public class HellfireMissileLauncher : Weapon
         {
             missiles.Add(missilePool.Create(Player, 0, MuzzleTrans.position) as HellfireMissileBehaviour);
         }
-        if (Physics.Raycast(Player.transform.position, Player.transform.forward, out hitInfo))
+        if (Physics.Raycast(Player.transform.position, Player.transform.forward, out hitInfo)
+            && hitInfo.collider.tag == "Enemy")
         {
             foreach (HellfireMissileBehaviour missile in missiles)
             {
@@ -88,11 +92,21 @@ public class HellfireMissileLauncher : Weapon
     {
         if (!gameObject.transform.parent.gameObject.GetComponent<NetworkIdentity>().isLocalPlayer) return;
 
+
         this.timer += Time.deltaTime;
-        if (Input.GetKey(KeyCode.Mouse0) && (this.timer >= this.fireRate))
+        reticle.color = Color.white;
+        if (this.timer >= this.fireRate)
         {
-            Fire();
-            this.timer = 0.0f;
+            RaycastHit hitInfo;
+            if (Physics.Raycast(Player.transform.position, Player.transform.forward, out hitInfo)
+            && hitInfo.collider.tag == "Enemy")
+                reticle.color = Color.red;
+
+            if (Input.GetKey(KeyCode.Mouse0))
+            {
+                Fire();
+                this.timer = 0.0f;
+            }
         }
         base.Update();
     }
